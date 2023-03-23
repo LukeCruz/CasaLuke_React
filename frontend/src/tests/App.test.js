afb53ea38a8a41476f3bdd4835cec618c6c1f2bc
Luke Cruz <enio.cruz@yahoo.com.br>
2023-03-23 04:39:17 -0300

    fix deploy

--- frontend/src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renderizada itens na tela', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Sobre Nós/);
-  expect(linkElement).toBeInTheDocument();
-});
